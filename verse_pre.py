0810940ab72790c6ac7b034d72e1161460e5b02d
ChrisXzz <xinzezhou97@gmail.com>
2024-05-22 14:18:39 -0400

    Add files via upload

---
+++ verse_pre.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import argparse
+
+def extract_and_rename_ct_files(source_dir, save_dir):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    for case_folder in os.listdir(source_dir):
+        case_path = os.path.join(source_dir, case_folder)
+        if os.path.isdir(case_path):
+            ct_file_path = os.path.join(case_path, 'ct.nii.gz')
+            if os.path.exists(ct_file_path):
+                new_ct_filename = f"{case_folder}_ct.nii.gz"
+                new_ct_file_path = os.path.join(save_dir, new_ct_filename)
+                
+                # Copy and rename the ct.nii.gz file to the destination directory
+                shutil.copy(ct_file_path, new_ct_file_path)
+            
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', default='/Users/chrischow/Downloads/WeaklyRevision/', help='The path of totalsegmentator data')
+    parser.add_argument('--save_dir', default='/Users/chrischow/test_out/', help='The saving path after reorganizing')
+    args = parser.parse_args()
+    
+    extract_and_rename_ct_files(args.data_path, args.save_dir)
+
+if __name__ == '__main__':
+    main()
+
